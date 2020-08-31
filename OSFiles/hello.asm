2b26e1d34e3cd29b2649fbc94411c924ac94a334
Rynder1 <liam.ellis37@gmail.com>
2020-08-31 13:30:17 +1000

    Added base files
    These are the base files in my OS building project.
    
    The boot file just shows the basic booting from hard disk
    
    The hello file shows the same but with the word Hello below it using system interrupts

---
+++ OSFiles/hello.asm
@@ -0,0 +1,20 @@
+mov ah, 0x0e  ; int
+
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+
+jmp $
+
+
+times 510-($-$$) db 0 ; Pad the boot sector out with zeros
+
+dw 0xaa55
+
